a9643aa52d9679c7669ff53e002101fbf1b2a8d6
Tbetcha <troy.boettger@gmail.com>
2020-02-23 09:13:56 -0500

    updated picture to show, knows correct answer and updated gameplay stuff

--- app/src/main/java/com/tbetcha/guessme/MainActivity.java
+++ app/src/main/java/com/tbetcha/guessme/MainActivity.java
@@ -3,27 +3,44 @@ package com.tbetcha.guessme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Integer ansPics[] = {R.drawable.coffee, R.drawable.juice, R.drawable.milk, R.drawable.water, R.drawable.soda_cans};
+    private Integer ansPics[] = {R.drawable.item_3, R.drawable.item_0, R.drawable.item_1, R.drawable.item_2};
     private int currAnsPic = 0;
 
-    private Button SubmitButton;
+    private Button submitButton;
     private EditText typeGuess;
     private ImageView strikeOne;
     private ImageView strikeTwo;
     private ImageView showItem;
+    private String currAns;
+    private String guess;
+    private boolean hasOneStrike = false;
+    private boolean hasTwoStrike = false;
+    private boolean corrAns = false;
+
+   //TODO fix guess line so it has a hint and not solid text
+
+
+    public int makeRandNumber() {
+        Random r = new Random();
+        currAnsPic = r.nextInt(4);
+        return currAnsPic;
+    }
 
     private Question[] questions = new Question[]{
-            new Question(0,"MILK"),
+            new Question(0, "MILK"),
             new Question(1, "WATER"),
             new Question(2, "SODA"),
-            new Question(3,"JUICE" ),
-            new Question(4, "COFEE")
+            new Question(3, "JUICE"),
     };
 
 
@@ -31,8 +48,112 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        showItem = (ImageView) findViewById(R.id.showItem);
+        submitButton = (Button) findViewById(R.id.submit_button);
+        strikeOne = (ImageView) findViewById(R.id.strike_one);
+        strikeTwo = (ImageView) findViewById(R.id.strike_two);
 
+
+        typeGuess = (EditText) findViewById(R.id.enter_guess);
+        typeGuess.setHint("Guess here");
+        begin();
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getAnswer();
+                checkRealAns();
+
+
+            }
+        });
 
 
     }
+
+    public void nextQuestion(int random) {
+        makeRandNumber();
+        currAns = questions[currAnsPic].getName();
+    }
+
+    public void checkRealAns() {
+        if (getAnswer().equals(questions[currAnsPic].getName())) {
+            correct();
+            Toast.makeText(this, "WOOHOOO", Toast.LENGTH_SHORT).show();
+            if (questions[currAnsPic].getQuestion() == 0) {
+                showItem.setImageResource(R.drawable.item_0);
+                showItem.setVisibility(View.VISIBLE);
+            } else if (questions[currAnsPic].getQuestion() == 1) {
+                showItem.setImageResource(R.drawable.item_1);
+                showItem.setVisibility(View.VISIBLE);
+            } else if (questions[currAnsPic].getQuestion() == 2) {
+                showItem.setImageResource(R.drawable.item_2);
+                showItem.setVisibility(View.VISIBLE);
+            } else if (questions[currAnsPic].getQuestion() == 3) {
+                showItem.setImageResource(R.drawable.item_3);
+                showItem.setVisibility(View.VISIBLE);
+            }
+
+
+            nextQuestion(makeRandNumber());
+             new java.util.Timer().schedule(
+                    new java.util.TimerTask() {
+                        @Override
+                        public void run() {
+                            begin();
+                        }
+                    },
+                    2000);
+
+             //TODO clear text from guess line at this point
+
+        } else {
+            whichWrong();
+        }
+    }
+
+    public void whichWrong() {
+        if (!hasOneStrike && !hasTwoStrike) {
+            hasOneStrike = true;
+            hasTwoStrike = false;
+            corrAns = false;
+            strikeOne.setVisibility(View.VISIBLE);
+            strikeTwo.setVisibility(View.INVISIBLE);
+            Toast.makeText(this, "one strike", Toast.LENGTH_SHORT).show();
+        } else if (hasOneStrike && !hasTwoStrike) {
+            hasOneStrike = true;
+            hasTwoStrike = true;
+            corrAns = false;
+            strikeTwo.setVisibility(View.VISIBLE);
+        } else {
+            Toast.makeText(this, "You struck out", Toast.LENGTH_SHORT).show();
+            begin();
+
+        }
+    }
+
+    public String getAnswer() {
+        guess = typeGuess.getText().toString().toUpperCase();
+        return guess;
+    }
+
+    public void begin() {
+         hasOneStrike = false;
+         hasTwoStrike = false;
+         corrAns = false;
+        strikeOne.setVisibility(View.INVISIBLE);
+        strikeTwo.setVisibility(View.INVISIBLE);
+        showItem.setVisibility(View.INVISIBLE);
+        nextQuestion(makeRandNumber());
+    }
+
+    public void correct(){
+        hasOneStrike = false;
+        hasTwoStrike = false;
+        corrAns = true;
+        strikeOne.setVisibility(View.INVISIBLE);
+        strikeTwo.setVisibility(View.INVISIBLE);
+    }
+
+
 }
--- app/src/main/java/com/tbetcha/guessme/Question.java
+++ app/src/main/java/com/tbetcha/guessme/Question.java
@@ -7,16 +7,45 @@ public class Question {
     private int question;
     private String name;
 
+
+
 public Question(int question, String name){
     this.question=question;
     this.name = name;
+
 }
 
-public int makeNumber(){
+public void oneWrong(){
+
+}
+
+public void twoWrong(){
+
+}
+
+    public void setName(int Question){
+        this.question = question;
+        this.name = name;
+
+    }
+
+    public String getName(){
+    return this.name;
+    }
+
+    public void rightAns(){
+
+    }
+
+    public int  getQuestion(){
+      return this.question;
+    }
+
+/*public int makeNumber(){     might not need this here - moved main activity
     Random r = new Random();
-    return  r.nextInt(4);
+    return  r.nextInt(4);*/
 }
 
 
 
-}
+
